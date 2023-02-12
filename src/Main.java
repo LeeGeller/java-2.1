d37c37f8eb2af67dc57e8372528afc1801aac985
Kristina Budnik <ssspers92@gmail.com>
2023-02-12 17:26:15 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int purchase;
+        int oneBonusRub;
+        int bonusMile;
+
+        purchase = 13676;
+        oneBonusRub = 20;
+        bonusMile = purchase/oneBonusRub;
+
+        System.out.println(bonusMile);
+    }
+}
